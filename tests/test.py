18af05cbc192773f0f684fa9e2077b238d50c700
안재현 <jaehyunahn@nexon.co.kr>
2019-01-14 18:45:49 +0900

    add under, over out lier test

--- tests/test.py
+++ tests/test.py
@@ -7,14 +7,16 @@ from statistics import mean, variance
 from CredibilityInterval import LogCredibilityInterval
 
 if __name__ == '__main__':
-    stop = 0.3
-    interval = '99%'
+    stop = 0.01
+    interval = '1-sigma'
     current_path = os.path.dirname(os.path.realpath(__file__))
     data_file = os.path.join(current_path, '../external_files/dummy.txt')
 
+    # functional test
     with open(data_file, 'r') as file:
         data = [int(line.rstrip()) for line in file]
-        count_outlier = 0
+        u_outlier = 0
+        o_outlier = 0
         for i in range(1, len(data)):
             cur_data = [log(x) for x in data[:i]]
             m = mean(cur_data)
@@ -27,12 +29,12 @@ if __name__ == '__main__':
                 current = cur_data[-1]
                 int_current = round(exp(current))
                 result = 'Current:\t{}\t\t({:.2f} < x < {:.2f})\t'.format(int_current, one_tail, two_tails)
-                if one_tail > int_current or two_tails < int_current:
-                    result += 'outlier!'
-                    count_outlier += 1
+                if one_tail > int_current:
+                    result += 'under outlier!'
+                    u_outlier += 1
+                elif two_tails < int_current:
+                    result += 'over outlier!'
+                    o_outlier += 1
                 print('{}'.format(result))
             time.sleep(stop)
-        print('{}/{}'.format(count_outlier, len(data)))
-
-            # functional test
-
+        print('{}/{} (outliers: {}, {})'.format(u_outlier + o_outlier, len(data), u_outlier, o_outlier))
